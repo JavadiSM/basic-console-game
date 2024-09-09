159dccbc6ce8318c53744b8e80065a77547f6d65
JavadiSM <1remehr@gmail.com>
2024-09-09 10:46:26 +0330

    hat printer created, I made main.py as the main program and the entry pont to program

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import re
+from cmd_color_printer import ColorPrinter, print_hat
+def main():
+    os.system("cls")
+    print_hat()
+    while not re.match("^exit$",user_command:=input()):
+        pass
+    os.system("cls")
+
+
+if __name__ == "__main__":
+    main()
